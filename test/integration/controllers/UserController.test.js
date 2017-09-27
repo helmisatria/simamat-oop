05ea2a47aafd697d690492612aaca50fbf12f6a6
Helmi Satria <satriahelmi@gmail.com>
2017-09-27 07:20:47 +0700

    testing

---
+++ test/integration/controllers/UserController.test.js
@@ -0,0 +1,16 @@
+var request = require('supertest');
+
+describe('UserController', function() {
+
+  describe('#login()', function() {
+    it('should redirect to /dashboard', function (done) {
+      request(sails.hooks.http.app)
+        .post('/login')
+        .send({ username: 'asd', password: 'asd' })
+        .expect(200)
+        .expect('location','/dashboard')
+        .end(done)
+    });
+  });
+
+});
